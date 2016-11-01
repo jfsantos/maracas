e96446bccd867a0136c2028276d5c14093479888
Joao Felipe Santos <joao.eel@gmail.com>
2016-11-01 12:55:06 -0400

    Fixed tests to use utils.wavread

--- tests/test_add_noise.py
+++ tests/test_add_noise.py
@@ -1,13 +1,11 @@
 from maracas import add_noise, asl_meter, rms_energy
-from scipy.io.wavfile import read as wavread
+from maracas.utils import wavread
+
 import numpy as np
 
 def test_add_noise_rms():
-    fs, x = wavread('tests/sp10.wav')
-    _, n = wavread('tests/ssn.wav')
-
-    x = x / 2.0**15
-    n = n / 2.0**15
+    x, fs = wavread('tests/sp10.wav')
+    n, _ = wavread('tests/ssn.wav')
 
     y, n_scaled = add_noise(x, n, fs, 5.0)
     snr = 20*np.log10(np.linalg.norm(x)/np.linalg.norm(n_scaled))
@@ -16,11 +14,8 @@ def test_add_noise_rms():
 
 
 def test_add_noise_p56():
-    fs, x = wavread('tests/sp10.wav')
-    _, n = wavread('tests/ssn.wav')
-
-    x = x / 2.0**15
-    n = n / 2.0**15
+    x, fs = wavread('tests/sp10.wav')
+    n, _ = wavread('tests/ssn.wav')
 
     y, n_scaled = add_noise(x, n, fs, 5.0, speech_energy='P.56')
     snr = asl_meter(x, fs) - rms_energy(n_scaled)
--- tests/test_asl_meter.py
+++ tests/test_asl_meter.py
@@ -1,10 +1,11 @@
 from maracas import asl_meter
-from scipy.io.wavfile import read as wavread
+from maracas.utils import wavread
+
 import numpy as np
 
 def test_asl_meter():
-    fs, x = wavread('tests/sp10.wav')
-    assert np.isclose(asl_meter(x, fs), -25.631646819880210)
+    x, fs = wavread('tests/sp10.wav')
+    assert np.isclose(asl_meter(x, fs), -25.631381743520010)
 
 if __name__ == '__main__':
     test_asl_meter()
